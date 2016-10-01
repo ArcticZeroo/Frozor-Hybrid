ffad9385b78e62fa09febd903d03ce0c44ed3660
ArcticZeroo <spencer@artix.cc>
2016-10-01 15:45:40 -0400

    Move MinecraftBot to own module

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var MinecraftBot = require('./minecraft/MinecraftBot');
 var Commands     = require('./commands/Commands');
 var SlackBot     = require('./slack/SlackBot');
 var Objects      = require('./objects/Objects');
 
 module.exports = {
     Objects:      Objects,
-    MinecraftBot: MinecraftBot,
     SlackBot:     SlackBot,
     Commands:     Commands
 };
