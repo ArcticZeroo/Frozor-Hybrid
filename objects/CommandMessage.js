cc2c51dfbfa3a00264084c946d3ea126ba29b8d5
ArcticZeroo <spencer@artix.cc>
2016-09-30 21:53:09 -0400

    Make CommandMessage work w/ args_before of !=0

--- objects/CommandMessage.js
+++ objects/CommandMessage.js
@@ -1,21 +1,23 @@
 var SlackMessage = require('./SlackMessage');
 var User         = require('./User');
 
+var log = require('frozor-logger')
+
 class SlackCommand extends SlackMessage{
     /**
      *
      * @param message - A JSON message object from the slack API
-     * @param splice_count - The amount of args (words) before the actual command. This is 1 by default because the default usage for commands is @botuser command_name args
+     * @param args_before - The amount of args (words) before the actual command. This is 1 by default because the default usage for commands is @botuser command_name args
      */
 
     constructor(message, args_before){
-        args_before = args_before || 1;
+        args_before = (args_before != undefined) ? args_before : 1;
 
         super(message);
 
         this._args    = this.getText().split(' ');
 
-        for(var arg in this._args) if(this._args[arg].trim() == '') args.splice(arg, 1);
+        for(var arg in this._args) if(this._args[arg].trim() == '') this._args.splice(arg, 1);
 
         this._name    = this._args[args_before];
 
