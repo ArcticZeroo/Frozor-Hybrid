66909021a3359f64e56a9b54281c1cb19850c63c
ArcticZeroo <spencer@artix.cc>
2016-10-01 15:58:58 -0400

    Remove old stuff

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var Commands     = require('./commands/Commands');
-var SlackBot     = require('./slack/SlackBot');
 var Objects      = require('./objects/Objects');
 
 module.exports = {
     Objects:      Objects,
-    SlackBot:     SlackBot,
     Commands:     Commands
 };
